01ce046a5567b03b6eec6ef39e82c954b3c8da31
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2020-03-09 17:35:01 +0100

    New parameter estimations

--- src/poisson_binomial.jl
+++ src/poisson_binomial.jl
@@ -2,7 +2,6 @@ using DataFrames
 using CSV
 using Distributions
 
-df = "./data/time_series/diff/SARS.csv" |> CSV.File |> DataFrame
 
 function probability_patient_dies_within_k_periods(k, α, p)
     return α * (1 - (1 - p)^k)
@@ -13,19 +12,22 @@ function confidence_interval(confidence::Int)::Vector{Float64}
     return [(1 - confidence) / 2, confidence + (1 - confidence) / 2]
 end
 
-function calculate_admissible_parameters(name, df, confidence)
-    admissible_parameters = []
+function calculate_admissible_parameters(name, censor_end, confidence)
+    df = "./data/time_series/diff/$name.csv" |> CSV.File |> DataFrame
 
-    α_space = 0.05:0.01:0.15
+    α_space = 0.01:0.01:0.1
     p_space = 0.01:0.01:1
 
-    censor_end = 14
+    CI = confidence_interval(confidence)
 
     # E = epidemic curve
     E = df.Infected[1:censor_end]
+    println(E)
 
     # y = real death count
     true_deaths = sum(df.Dead[1:censor_end])
+    println(true_deaths)
+
 
     function build_poisson_binomial_distribution(E, α, p)
         # size of the probability vector is the total number of infected patients
@@ -47,28 +49,33 @@ function calculate_admissible_parameters(name, df, confidence)
         return PoissonBinomial(probability_vector)
     end
 
+    admissible_parameters = []
     for α in α_space
         for p in p_space
-            println("(α=$α, p=$p) Building Poisson Binomial distribution...")
+            println("[$name] (α=$α, p=$p) Building Poisson Binomial distribution...")
             poisson_binomial_distribution = build_poisson_binomial_distribution(E, α, p)
-            println("(α=$α, p=$p) Created distribution with μ=$(mean(poisson_binomial_distribution)) and σ^2=$(var(poisson_binomial_distribution))")
+            println("[$name] (α=$α, p=$p) Poisson Binomial distribution: μ=$(mean(poisson_binomial_distribution)), σ^2=$(var(poisson_binomial_distribution))")
 
-            println(confidence_interval(confidence))
-
-            low_deaths, high_deaths = quantile.(poisson_binomial_distribution, confidence_interval(confidence))
+            low_deaths, high_deaths = quantile.(poisson_binomial_distribution, CI)
 
             if low_deaths <= true_deaths <= high_deaths
-                println("(α=$α, p=$p) Parameters are admissible!")
+                println("[$name] (α=$α, p=$p) Parameters are admissible! [$low_deaths, $high_deaths]")
                 push!(admissible_parameters, (α, p))
             end
         end
     end
 
-    println("Writing parameters to file.")
+    println("[$name] Writing parameters to file.")
 
-    admissible_parameters |> DataFrame |> rename([1 => "α", 2 => "p"]) |> CSV.write("./results/$name_$(censor_end)d_$(confidence)_params.csv")
+    rename(DataFrame(admissible_parameters), [1 => "α", 2 => "p"]) |> CSV.write("./results/$(name)_$(censor_end)d_$(confidence)_params.csv")
 
     return admissible_parameters
 end
 
-calculate_admissible_parameters("SARS", df, 95)
+# calculate_admissible_parameters("SARS", 7, 90)
+# calculate_admissible_parameters("SARS", 14, 90)
+# calculate_admissible_parameters("SARS", 21, 90)
+
+calculate_admissible_parameters("Coronavirus", 7, 90)
+calculate_admissible_parameters("Coronavirus", 14, 90)
+calculate_admissible_parameters("Coronavirus", 21, 90)
