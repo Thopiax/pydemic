3fc4af24b55129a33011b87b446331d56af2a0d2
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2020-03-19 20:08:32 +0100

    Minimax model implemented

--- src/poisson_binomial.jl
+++ src/poisson_binomial.jl
@@ -21,12 +21,21 @@ end
 
 function log_likelihood(E, y, α, p)
     T = size(E, 1)
-    return sum([pdf.(daily_fatality_distribution(E[k], T - k, α, p), round(Int, y[k])) for k in 1:(T - 1)])
+    return sum([
+        log(pdf.(
+            daily_fatality_distribution(E[k], T - k, α, p),
+            round(Int, y[k])
+        )) for k in 1:(T - 1)
+    ])
 end
 
 function confidence_interval(confidence::Int)::Vector{Float64}
     confidence /= 100
     return [(1 - confidence) / 2, confidence + (1 - confidence) / 2]
+end
+
+function filterparams!(param_df, threshold)
+    filter!(row -> row[:d] <= threshold, param_df);
 end
 
 function calculate_admissible_parameters(name, censor_end, confidence, α_space, p_space)
@@ -49,6 +58,8 @@ function calculate_admissible_parameters(name, censor_end, confidence, α_space,
 
             println("[$name] (α=$α, p=$p) Total Fatality samples: μ=$(mean(total_fatality_samples)), σ^2=$(var(total_fatality_samples))")
 
+            println("")
+
             low_deaths, high_deaths = quantile(total_fatality_samples, [low_confidence_threshold,  high_confidence_threshold])
 
             if low_deaths <= true_deaths <= high_deaths
@@ -67,13 +78,82 @@ function calculate_admissible_parameters(name, censor_end, confidence, α_space,
     return admissible_parameters
 end
 
+function minimax_parameter_search(name, T, α_space, p_space, admissibility_window=0.1, nsims=10000)
+    df = "./data/time_series/diff/$name.csv" |> CSV.File |> DataFrame
+
+    @assert T <= size(df, 1)
+    @assert 0 < admissibility_window < 1
+
+    X = cumsum(df.Dead)
+
+    param_df = DataFrame(OrderedDict("α" => [],"p" => [], "d" => []))
+    minimum_distance = nothing
+
+    for α in α_space
+        minimum_distance_for_alpha = nothing
+        for p in p_space
+            maximum_distance_for_params = -1
+
+            for k in 1:T
+                E = df.Infected[1:k] # E = epidemic curve
+
+                total_fatality_samples = simulate_total_fatality_samples(E, nsims, α, p)
+                expected_total_fatalities = mean(total_fatality_samples)
+
+                actual_distance = abs(X[k] - expected_total_fatalities)
+
+                if maximum_distance_for_params < actual_distance
+                    maximum_distance_for_params = actual_distance
+                end
+            end
+
+            if isnothing(minimum_distance)
+                minimum_distance = maximum_distance_for_params
+            end
+
+            if isnothing(minimum_distance_for_alpha) || maximum_distance_for_params <= minimum_distance_for_alpha
+                minimum_distance_for_alpha = maximum_distance_for_params
+            end
+
+            if maximum_distance_for_params <= minimum_distance
+                minimum_distance = maximum_distance_for_params
+
+                println("[$name] (α=$α, p=$p) New minimum distance $minimum_distance")
+
+                filterparams!(param_df, (1 + admissibility_window) * minimum_distance)
+            end
+
+
+            if maximum_distance_for_params <= (1 + admissibility_window) * minimum_distance
+                println("[$name] (α=$α, p=$p) Admissible Parameter
+                        size(param_df)=$(size(param_df, 1))
+                ")
+
+                push!(param_df, [α, p, maximum_distance_for_params])
+            end
+        end
+
+        println("[$name] Minimum Distance for α=$α is $(minimum_distance_for_alpha)")
+    end
+
+    println("[$name] Writing parameters to file.")
+
+    param_df |> CSV.write("./results/minimax/$(name)_$(T)d_$(round(Int, admissibility_window * 100))_params.csv")
+
+    return param_df
+end
+
+
 SARS_α_space = 0.05:0.001:0.15
-SARS_p_space = 0:0.01:1
+SARS_p_space = 0:0.01:0.3
 
 calculate_admissible_parameters("SARS", 7, 90, SARS_α_space, SARS_p_space)
 calculate_admissible_parameters("SARS", 14, 90, SARS_α_space, SARS_p_space)
 calculate_admissible_parameters("SARS", 21, 90, SARS_α_space, SARS_p_space)
 calculate_admissible_parameters("SARS", 117, 90, SARS_α_space, SARS_p_space)
+
+minimax_parameter_search("SARS", 7, SARS_α_space, SARS_p_space)
+minimax_parameter_search("SARS", 21, SARS_α_space, SARS_p_space, 0.2)
 
 # MERS
 
@@ -92,10 +172,10 @@ calculate_admissible_parameters("MERS_second", 436, 90, MERS_α_space, MERS_p_sp
 
 # Coronavirus
 
-Corona_α_space = 0.01:0.001:0.1
-Corona_p_space = 0:0.01:1
+Corona_α_space = 0.01:0.005:0.1
+Corona_p_space = 0:0.05:1
 
-calculate_admissible_parameters("Coronavirus", 7, 20, Corona_α_space, Corona_p_space)
-calculate_admissible_parameters("Coronavirus", 14, 20, Corona_α_space, Corona_p_space)
-calculate_admissible_parameters("Coronavirus", 21, 20, Corona_α_space, Corona_p_space)
-calculate_admissible_parameters("Coronavirus", 29, 20, Corona_α_space, Corona_p_space)
+calculate_admissible_parameters("Coronavirus", 7, 90, Corona_α_space, Corona_p_space)
+calculate_admissible_parameters("Coronavirus", 14, 90, Corona_α_space, Corona_p_space)
+calculate_admissible_parameters("Coronavirus", 21, 90, Corona_α_space, Corona_p_space)
+calculate_admissible_parameters("Coronavirus", 29, 90, Corona_α_space, Corona_p_space)
