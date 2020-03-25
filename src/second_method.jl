e41f4eec3468e72c98bc9c30645cecd74bfff7b0
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2020-03-25 10:55:54 +0100

    Implement FFX and run algorithm form many more cases.

--- src/second_method.jl
+++ src/second_method.jl
@@ -1,17 +1,19 @@
 using DataFrames
 using CSV
 using Distributions
+using DataStructures
 
-function daily_fatality_distribution(n, k, α, p)
+function fatality_distribution(x, k, α, p)
+    n = round(Int, x)
     return Binomial(n, α * (1 - (1 - p)^k))
 end
 
-function simulate_total_fatality_samples(E, nsamples, α, p)
+function cumulative_fatality_samples(E, nsamples, α, p)
     T = size(E, 1)
     ans = Array{Float64, 2}(undef, nsamples, T)
 
     for (t, n) in enumerate(E)
-        D_t = daily_fatality_distribution(n, T - t, α, p)
+        D_t = fatality_distribution(n, T - t, α, p)
 
         ans[:, t] = rand(D_t, nsamples)
     end
@@ -23,7 +25,7 @@ function log_likelihood(E, y, α, p)
     T = size(E, 1)
     return sum([
         log(pdf.(
-            daily_fatality_distribution(E[k], T - k, α, p),
+            fatality_distribution(E[k], T - k, α, p),
             round(Int, y[k])
         )) for k in 1:(T - 1)
     ])
@@ -38,132 +40,138 @@ function filterparams!(param_df, threshold)
     filter!(row -> row[:d] <= threshold, param_df);
 end
 
-function calculate_admissible_parameters(name, censor_end, confidence, α_space, p_space)
-    df = "./data/time_series/diff/$name.csv" |> CSV.File |> DataFrame
+function max_distance(E, X, T, nsims, α, p)
+    maximum_distance = -1
+    for k in 1:T
+        total_fatality_samples = cumulative_fatality_samples(E[1:k], nsims, α, p)
+        expected_total_fatalities = mean(total_fatality_samples)
 
-    low_confidence_threshold, high_confidence_threshold = confidence_interval(confidence)
+        actual_distance = abs(X[k] - expected_total_fatalities)
 
-    # E = epidemic curve
-    E = df.Infected[1:censor_end]
-
-    # y = real death count
-    y = df.Dead[1:censor_end]
-
-    true_deaths = sum(y)
-
-    admissible_parameters = []
-    for α in α_space
-        for p in p_space
-            total_fatality_samples = simulate_total_fatality_samples(E, 10000, α, p)
-
-            println("[$name] (α=$α, p=$p) Total Fatality samples: μ=$(mean(total_fatality_samples)), σ^2=$(var(total_fatality_samples))")
-
-            println("")
-
-            low_deaths, high_deaths = quantile(total_fatality_samples, [low_confidence_threshold,  high_confidence_threshold])
-
-            if low_deaths <= true_deaths <= high_deaths
-                l = log_likelihood(E, y, α, p)
-                println("[$name] (α=$α, p=$p) Likelihood=$l")
-                println("[$name] (α=$α, p=$p) Parameters are admissible! [$low_deaths, $high_deaths]")
-                push!(admissible_parameters, (α, p, l))
-            end
+        if actual_distance > maximum_distance
+            ans = actual_distance
         end
     end
 
-    println("[$name] Writing parameters to file.")
-
-    rename(DataFrame(admissible_parameters), [1 => "α", 2 => "p", 3 => "l"]) |> CSV.write("./results/$(name)_$(censor_end)d_$(confidence)_params.csv")
-
-    return admissible_parameters
+    return maximum_distance
 end
 
-function minimax_parameter_search(name, T, α_space, p_space, admissibility_window=0.1, nsims=10000)
+function mean_distance(E, X, T, nsims, α, p)
+    distances = []
+    for k in 1:T
+        total_fatality_samples = cumulative_fatality_samples(E[1:k], nsims, α, p)
+        expected_total_fatalities = mean(total_fatality_samples)
+
+        push!(distances, abs(X[k] - expected_total_fatalities))
+    end
+
+    return mean(distances)
+end
+
+function choose_metric(metric)
+    if metric == "mean"
+        return mean_distance
+    elseif metric == "max"
+        return max_distance
+    end
+
+    # defaults to mean distance
+    return mean_distance
+end
+
+# Returns the day where the Xth case occured
+function FFXidx(X, E)
+    sum = 0
+
+    for (i, e) in enumerate(E)
+        sum += e
+
+        if sum >= X
+            return i
+        end
+    end
+
+    return size(E, 1)
+end
+
+function new_parameter_search(name, T, α_space, p_space, admissibility_window=0.1, FFX=false, nsims=10000, distance_metric="mean")
     df = "./data/time_series/diff/$name.csv" |> CSV.File |> DataFrame
 
-    @assert T <= size(df, 1)
+    if FFX
+        X = T
+        T = FFXidx(X, df.Infected)
+
+        censor_string = "$(X)FFX"
+        println("Case #$X occured on day $T after infection")
+    else
+        censor_string = "$(T)d"
+    end
+
+    @assert 0 < T <= size(df, 1)
     @assert 0 < admissibility_window < 1
 
-    X = cumsum(df.Dead)
+    E = copy(df.Infected[1:T]) # epidemic curve
+    X = cumsum(df.Dead)[1:T] # cumulative mortality curve
 
     param_df = DataFrame(OrderedDict("α" => [],"p" => [], "d" => []))
     minimum_distance = nothing
 
     for α in α_space
-        minimum_distance_for_alpha = nothing
+        println("[$name] Running for α=$α")
         for p in p_space
-            maximum_distance_for_params = -1
+            distance = choose_metric(distance_metric)(E, X, T, nsims, α, p)
 
-            for k in 1:T
-                E = df.Infected[1:k] # E = epidemic curve
-
-                total_fatality_samples = simulate_total_fatality_samples(E, nsims, α, p)
-                expected_total_fatalities = mean(total_fatality_samples)
-
-                actual_distance = abs(X[k] - expected_total_fatalities)
-
-                if maximum_distance_for_params < actual_distance
-                    maximum_distance_for_params = actual_distance
-                end
-            end
-
-            if isnothing(minimum_distance)
-                minimum_distance = maximum_distance_for_params
-            end
-
-            if isnothing(minimum_distance_for_alpha) || maximum_distance_for_params <= minimum_distance_for_alpha
-                minimum_distance_for_alpha = maximum_distance_for_params
-            end
-
-            if maximum_distance_for_params <= minimum_distance
-                minimum_distance = maximum_distance_for_params
+            if isnothing(minimum_distance) || distance <= minimum_distance
+                minimum_distance = distance
 
                 println("[$name] (α=$α, p=$p) New minimum distance $minimum_distance")
 
                 filterparams!(param_df, (1 + admissibility_window) * minimum_distance)
             end
 
+            if distance <= (1 + admissibility_window) * minimum_distance
+                push!(param_df, [α, p, distance])
 
-            if maximum_distance_for_params <= (1 + admissibility_window) * minimum_distance
                 println("[$name] (α=$α, p=$p) Admissible Parameter
+                        distance=$(distance)
+                        threshold=$((1 + admissibility_window) * minimum_distance)
                         size(param_df)=$(size(param_df, 1))
                 ")
-
-                push!(param_df, [α, p, maximum_distance_for_params])
             end
         end
-
-        println("[$name] Minimum Distance for α=$α is $(minimum_distance_for_alpha)")
     end
 
     println("[$name] Writing parameters to file.")
 
-    param_df |> CSV.write("./results/minimax/$(name)_$(T)d_$(round(Int, admissibility_window * 100))_params.csv")
+    param_df |> CSV.write("./results/second_method/$(name)_$(censor_string)_$(round(Int, admissibility_window * 100))_$(distance_metric)_params.csv")
 
     return param_df
 end
 
 
 SARS_α_space = 0.05:0.001:0.15
-SARS_p_space = 0:0.01:0.3
+SARS_p_space = 0:0.02:1
 
 calculate_admissible_parameters("SARS", 7, 90, SARS_α_space, SARS_p_space)
 calculate_admissible_parameters("SARS", 14, 90, SARS_α_space, SARS_p_space)
 calculate_admissible_parameters("SARS", 21, 90, SARS_α_space, SARS_p_space)
 calculate_admissible_parameters("SARS", 117, 90, SARS_α_space, SARS_p_space)
 
-minimax_parameter_search("SARS", 7, SARS_α_space, SARS_p_space)
-minimax_parameter_search("SARS", 21, SARS_α_space, SARS_p_space, 0.2)
+new_parameter_search("SARS", 100, SARS_α_space, SARS_p_space, 0.2, true)
+new_parameter_search("SARS", 1000, SARS_α_space, SARS_p_space, 0.2, true)
+new_parameter_search("SARS", 5000, SARS_α_space, SARS_p_space, 0.2, true)
+new_parameter_search("SARS", 10000, SARS_α_space, SARS_p_space, 0.2, true)
 
 # MERS
 
-MERS_α_space = 0.20:0.01:0.40
-MERS_p_space = 0:0.01:1
+MERS_α_space = 0.20:0.01:0.30
+MERS_p_space = 0:0.02:1
 
-calculate_admissible_parameters("MERS_first", 7, 90, MERS_α_space, MERS_p_space)
-calculate_admissible_parameters("MERS_first", 14, 90, MERS_α_space, MERS_p_space)
-calculate_admissible_parameters("MERS_first", 21, 90, MERS_α_space, MERS_p_space)
-calculate_admissible_parameters("MERS_first", 402, 90, MERS_α_space, MERS_p_space)
+new_parameter_search("MERS_first", 100, MERS_α_space, MERS_p_space, 0.2, true)
+new_parameter_search("MERS_first", 1000, MERS_α_space, MERS_p_space, 0.2, true)
+
+new_parameter_search("MERS_second", 100, MERS_α_space, MERS_p_space, 0.2, true)
+new_parameter_search("MERS_second", 1000, MERS_α_space, MERS_p_space, 0.2, true)
 
 calculate_admissible_parameters("MERS_second", 7, 90, MERS_α_space, MERS_p_space)
 calculate_admissible_parameters("MERS_second", 14, 90, MERS_α_space, MERS_p_space)
@@ -173,11 +181,14 @@ calculate_admissible_parameters("MERS_second", 436, 90, MERS_α_space, MERS_p_sp
 # Coronavirus
 
 Corona_α_space = 0.01:0.001:0.1
-Corona_p_space = 0:0.01:1
+Corona_p_space = 0:0.02:1
 
-calculate_admissible_parameters("Coronavirus", 7, 90, Corona_α_space, Corona_p_space)
-calculate_admissible_parameters("Coronavirus", 14, 90, Corona_α_space, Corona_p_space)
-calculate_admissible_parameters("Coronavirus", 21, 90, Corona_α_space, Corona_p_space)
-calculate_admissible_parameters("Coronavirus", 29, 90, Corona_α_space, Corona_p_space)
+# calculate_admissible_parameters("Coronavirus", 7, 90, Corona_α_space, Corona_p_space)
+# calculate_admissible_parameters("Coronavirus", 14, 90, Corona_α_space, Corona_p_space)
+# calculate_admissible_parameters("Coronavirus", 21, 90, Corona_α_space, Corona_p_space)
+# calculate_admissible_parameters("Coronavirus", 29, 90, Corona_α_space, Corona_p_space)
 
-minimax_parameter_search("Coronavirus", 21, Corona_α_space, Corona_p_space, 0.2)
+new_parameter_search("Coronavirus", 100, Corona_α_space, Corona_p_space, 0.2, true)
+new_parameter_search("Coronavirus", 1_000, Corona_α_space, Corona_p_space, 0.2, true)
+new_parameter_search("Coronavirus", 10_000, Corona_α_space, Corona_p_space, 0.2, true)
+new_parameter_search("Coronavirus", 100_000, Corona_α_space, Corona_p_space, 0.2, true)
